d0b7972358c3d20ed5dd88195a6958180a696331
Bl1nker <Alex_001@sibmail.com>
2024-09-26 15:11:55 +0700

    task 4 done. all done

---
+++ Task_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void ReplaceNegativeNumbers(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        // Введите свое решение ниже
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (array[i, j] < 0) array[i, j] = -array[i, j];
+            }
+        }
+    }
+
+    static void ShowArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main()
+    {
+        int[,] array =
+        {
+            { 1, -2, 3 },
+            { -4, 5, -6 },
+            { 7, -8, 9 }
+        };
+        ReplaceNegativeNumbers(array);
+        ShowArray(array);
+    }
+}
