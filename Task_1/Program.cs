ba3e92983d558e902b05ab6f0e7a2a02fc82e220
Bl1nker <Alex_001@sibmail.com>
2024-09-26 14:29:51 +0700

    Task 1 done

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void FindMaxInRows(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+
+        for (int i = 0; i < rows; i++)
+        {
+            int max = array[i, 0];
+            for (int j = 0; j < cols; j++)
+            {
+                if (array[i, j] > max) max = array[i, j];
+            }
+            Console.WriteLine($"Максимальный элемент в строке {i}: {max}");
+        }
+    }
+    static void Main()
+    {
+        int[,] array =
+        {
+            { 9, 3, 5, 7 },
+            { 2, 4, 22, 8 },
+            { 9, 10, 11, 30 }
+        };
+        FindMaxInRows(array);
+    }
+}
