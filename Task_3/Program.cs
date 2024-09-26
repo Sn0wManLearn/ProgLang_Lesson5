cff14425af76639a19379cea1643271fa122ef42
Bl1nker <Alex_001@sibmail.com>
2024-09-26 14:43:13 +0700

    Task 3 done

---
+++ Task_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void TransposeMatrix(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        int[,] transposed = new int[cols, rows];
+        
+        for (int i = 0; i < cols; i++){
+            for (int j = 0; j < rows; j++){
+                transposed[i, j] = array[j, i];
+            }
+        }
+
+        for (int i = 0; i < cols; i++){
+            for (int j = 0; j < rows; j++){
+                Console.Write(transposed[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main()
+    {
+        int[,] array =
+        {
+            { 1, 2 },
+            { 3, 4 },
+            { 5, 6 }
+        };
+        TransposeMatrix(array);
+    }
+}
