8b6ce89776d0e314b4f5d9da1e6ba7db62822f40
Bl1nker <Alex_001@sibmail.com>
2024-09-26 14:35:56 +0700

    Task 2 done

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void FindSumInColumns(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        for (int i = 0; i < cols; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < rows; j++)
+            {
+                sum += array[j, i];
+            }
+            Console.WriteLine($"Сумма элементов в столбце {i}: {sum}");
+        }
+    }
+    static void Main()
+    {
+        int[,] array =
+        {
+            { 5, 2, 8 },
+            { 4, 9, 6 },
+            { 7, 8, 9 },
+            { 10, 11, 12 }
+        };
+        FindSumInColumns(array);
+    }
+}
